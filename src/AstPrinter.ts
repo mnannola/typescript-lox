305579cd5f1fea4ffa9609e6714f34cdc1bf837d
Michael Nannola <mnannola@legalzoom.com>
2023-05-31 22:33:12 -0500

    fixed astPrinter

--- src/AstPrinter.ts
+++ src/AstPrinter.ts
@@ -4,36 +4,36 @@ import { TokenType } from "./TokenType";
 
 class AstPrinter implements Visitor<String> {
     visitBinaryExpr(expr: Binary): String {
-        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+        return this.parenthesize(expr.operator.lexeme, expr.left, expr.right);
     }
     visitGroupingExpr(expr: Grouping): String {
-        return parenthesize("group", expr.expression);
+        return this.parenthesize("group", expr.expression);
     }
     visitLiteralExpr(expr: Literal): String {
         if (expr.value === null) return "nil";
         return expr.value.toString();
     }
     visitUnaryExpr(expr: Unary): String {
-        return parenthesize(expr.operator.lexeme, expr.right);
+        return this.parenthesize(expr.operator.lexeme, expr.right);
     }
     print(expr: Expr): String {
         return expr.accept(this);
     }
-}
-
-function parenthesize(name: String, ...exprs: Array<Expr>): String {
-    const builderArr = [];
-
-    builderArr.push("(");
-    builderArr.push(name);
-
-    for (const expr of exprs) {
-        builderArr.push(" ");
-        builderArr.push(expr.accept(this));        
+    
+    parenthesize(name: String, ...exprs: Expr[]): String {
+        const builderArr = [];
+    
+        builderArr.push("(");
+        builderArr.push(name);
+    
+        for (const expr of exprs) {
+            builderArr.push(" ");
+            builderArr.push(expr.accept(this));        
+        }
+        builderArr.push(")");
+    
+        return builderArr.join('').toString();
     }
-    builderArr.push(")");
-
-    return builderArr.join('').toString();
 }
 
 function main(): void {
@@ -46,6 +46,7 @@ function main(): void {
             new Literal(45.67)
         )
     );
+    
     console.log(new AstPrinter().print(expression));
 }
 
